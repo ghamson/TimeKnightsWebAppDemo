05ffd47a4a9f7707c03f8ea5463fa0164d98ee08
ghamson <ghamson@outlook.com>
2023-01-11 10:53:12 -0600

    Final Demo V1

---
+++ serviceworker.js
@@ -0,0 +1,19 @@
+// Perform install steps
+let CACHE_NAME = 'my-cache';
+let urlsToCache = [
+    'css/style.css',    '
+    'images/myIcon.png',
+    'scripts/index.js'
+
+    ];
+
+self.addEventListener('install', function(event) {
+// Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+        })
+    );
+});
